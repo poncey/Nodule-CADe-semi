46c77b98ff7ceccfeb0a47a0d09ec0538626d847
516936971 <wyh516936971@outlook.com>
2018-12-27 22:28:29 +0800

    updata luna_file_attributes.py

--- detector/luna_file_attributes.py
+++ detector/luna_file_attributes.py
@@ -12,4 +12,12 @@ np.random.shuffle(k)
 file_id = file_id[k]
 
 for i in range(k_fold):
-   pass
+    train_file_id = file_id[file_index!=i]
+    val_file_id = train_file_id[:88]
+    train_file_id = train_file_id[88:]
+    test_file_id = file_id[file_index==i]
+    if not os.path.exists('./luna_file_id/subset_{:d}'.format(i)):
+        os.mkdir('./luna_file_id/subset_{:d}'.format(i))
+    np.save("./luna_file_id/subset_{:d}".format(i) + "/train_file_id.npy",train_file_id)
+    np.save("./luna_file_id/subset_{:d}".format(i) + "/val_file_id.npy",val_file_id)
+    np.save("./luna_file_id/subset_{:d}".format(i) + "/test_file_id.npy",test_file_id)
