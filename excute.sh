eac49f7af9d94849e0089880b17c8431545343a3
516936971 <wyh516936971@outlook.com>
2018-12-31 14:17:30 +0800

    update data_prepare.py

--- excute.sh
+++ excute.sh
@@ -1,5 +1,5 @@
 # Excute codes in fold
 
-# python detection.py --model detector.res18 -b 16 --epochs 100 --save-dir res18 --save-freq 25 --test 1 --fold 0
-python detection.py --model detector.res18 -b 16 --epochs 100 --save-dir res18 --save-freq 25 --test 1 --fold 1
-python detection.py --model detector.res18 -b 16 --epochs 100 --save-dir res18 --save-freq 25 --test 1 --fold 2
+python detection.py --model detector.res18 -b 8 --epochs 100 --save-dir res18 --save-freq 25 --test 1 --fold 0
+python detection.py --model detector.res18 -b 8 --epochs 100 --save-dir res18 --save-freq 25 --test 1 --fold 1
+python detection.py --model detector.res18 -b 8 --epochs 100 --save-dir res18 --save-freq 25 --test 1 --fold 2
